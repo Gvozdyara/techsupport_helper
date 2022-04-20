ac72fd0f84211d9635de0547e5a8ad308c7baf3d
Gvozdyara <fahrutdinov.n@mail.ru>
2022-04-20 10:28:25 +0300

    added YaDisk synchronization

---
+++ yadsk.py
@@ -0,0 +1,24 @@
+import yadisk
+
+def upload():
+    global y
+    y = False
+    with open("ya_id.txt", "r") as f:
+        tok = f.read()
+    y = yadisk.YaDisk(token=tok)
+    print(y.check_token()) # Проверим токен
+    y.upload("techsupport_base", "/TSH/techsupport_base", overwrite=True)
+    return
+
+
+def download():
+    try:
+        with open("ya_id.txt", "r") as f:
+            tok = f.read()
+        y = yadisk.YaDisk(token=tok)
+        # Download "/some-file-to-download.txt" to "downloaded.txt"
+        y.download("/TSH/techsupport_base", "techsupport_base")
+        return "techsupport_base"
+    except yadisk.exceptions.PathNotFoundError:
+        return "techsupport_base"
+
